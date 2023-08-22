89d1ab41900ba42b329c19fa17b8d3e7b31c34e8
“wz” <1184321378@qq.com>
2023-08-22 23:55:45 +0800

    完成登录功能  需调整接口，验证

--- app/src/main/java/com/keys/iot/MainActivity.java
+++ app/src/main/java/com/keys/iot/MainActivity.java
@@ -2,13 +2,40 @@ package com.keys.iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.keys.iot.activity.LoginActivity;
+import com.keys.iot.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLogin;
+    private Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnRegister = findViewById(R.id.btn_register);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/keys/iot/config/AppConfig.java
@@ -0,0 +1,6 @@
+package com.keys.iot.config;
+
+public class AppConfig {
+
+    public static final String BASE_URI = "127.0.0.1";
+}
